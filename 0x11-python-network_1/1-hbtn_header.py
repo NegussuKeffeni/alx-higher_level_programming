6a86003949cc76f8740ebc89ece9de922331e832
NegussuKeffeni <nkeffeni@gmail.com>
2022-09-23 14:16:34 +0300

    fix everything

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,6 +2,7 @@
 """Take in a URL, send request to URL and display value of `X-Request-Id`"""
 import sys
 from urllib import request
+
 if __name__ == "__main__":
     with request.urlopen(sys.argv[1]) as res:
         print(res.info()['X-Request-Id'])
